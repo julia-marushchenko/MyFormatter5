c495f16e5d99279d873af8eb950adcb5da26ebf3
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-08-21 23:00:49 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/format/Main.java
@@ -0,0 +1,14 @@
+// Java program to format string representing unicode of chars
+
+package com.format;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Printing formated string
+        System.out.printf("%C%c%c%c%c%c", 65, 117, 103, 117, 115, 116); // Output: August
+    }
+}
